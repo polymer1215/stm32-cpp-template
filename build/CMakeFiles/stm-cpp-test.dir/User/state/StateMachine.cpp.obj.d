915df150670acfcac99693538feba21daddfb14c
Junye Peng <polymer0011@gmail.com>
2026-03-25 00:30:28 +0800

    清理build文件夹

--- build/CMakeFiles/stm-cpp-test.dir/User/state/StateMachine.cpp.obj.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/stm-cpp-test.dir/User/state/StateMachine.cpp.obj: \
- /Users/polymer/Dev/stm-cpp-test/User/state/StateMachine.cpp \
- /Users/polymer/Dev/stm-cpp-test/User/state/StateMachine.hpp \
- /Users/polymer/Dev/stm-cpp-test/User/state/State.hpp \
- /Users/polymer/Dev/stm-cpp-test/User/state/State_test.hpp
--- build/CMakeFiles/stm-cpp-test.dir/startup_stm32f103xb.s.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/stm-cpp-test.dir/startup_stm32f103xb.s.obj: \
- /Users/polymer/Dev/stm-cpp-test/startup_stm32f103xb.s
